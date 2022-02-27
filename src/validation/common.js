6f38476a15fd58e1c80b525c70f441f8b70b3cab
Jacob Daitzman <jdtzmn@gmail.com>
2022-02-26 23:28:50 -0500

    Support hyphenated schemes

--- src/validation/common.js
+++ src/validation/common.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 exports.registerSchema = Joi.object({
     protocol: Joi.string()
-        .regex(/^[a-zA-Z]+$/)
+        .regex(/^[a-zA-Z-]+$/)
         .required(),
     command: Joi.string().required(),
     override: Joi.boolean(),
